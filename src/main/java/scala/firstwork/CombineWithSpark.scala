998234334b82acc3be0cf70b363087bbea8f98c0
kingcall <2388054826@qq.com>
2018-01-23 14:35:07 +0800

    可打jar包可运行

--- src/main/java/scala/firstwork/CombineWithSpark.scala
+++ src/main/java/scala/firstwork/CombineWithSpark.scala
@@ -11,6 +11,7 @@ object CombineWithSpark {
   def main(args:Array[String]){
     println("开始接收数据了")
     val sparkConf = new SparkConf().setAppName("SqlNetworkWordCount").setMaster("local[2]")
+    sparkConf.set("spark.testing.memory", "2147480000")
     val scc=new StreamingContext(sparkConf,Seconds(2))
    /* scc.checkpoint("C:\\Users\\PLUSH80702\\Desktop\\tmp")*/
    val zkQuorum="192.168.10.74:2181,192.168.10.75:2181,192.168.10.76:2181,localhost:2181"
@@ -21,9 +22,10 @@ object CombineWithSpark {
       if (rdd.count()>0){
         rdd.foreach(x=>{
           var tmpobj=JSON.parseObject(x._2)
-          var name:String=tmpobj.get("host")+""
+          println(tmpobj)
+         /* var name:String=tmpobj.get("host")+""
           var conetxt:String=tmpobj.get("client_ip")+"\t"+tmpobj.get("is_blocked")+"\t"+tmpobj.get("args")+"\t"+tmpobj.get("status")+"\t"+tmpobj.get("uid")+"\t"+name+"\r\n"
-          SaveToLocal.saveFile(conetxt,name)
+          SaveToLocal.saveFile(conetxt,name)*/
 
         })
       }
